e5c35730ef939311ddd8e0fbba2deea1dd9f7466
599521964 <giorgimelikidze71@gmail.com>
2024-05-23 17:30:24 +0400

    second commit

---
+++ python cource/day 01/homework/lesson 01.py
@@ -0,0 +1,6 @@
+name = "Giorgi"
+surname = "Melikidze"
+age = 17
+height = 180
+knows_programming = True
+print(name + " " + surname +" "+ str(age) + " " + str(height) + " " + str(knows_programming))
