47ee19b83cafd628fecccd19d3c95cc114e86b75
599521964 <giorgimelikidze71@gmail.com>
2024-05-26 17:00:50 +0400

    three commit

--- python cource/day 01/homework/lesson 01.py
+++
@@ -1,6 +0,0 @@
-name = "Giorgi"
-surname = "Melikidze"
-age = 17
-height = 180
-knows_programming = True
-print(name + " " + surname +" "+ str(age) + " " + str(height) + " " + str(knows_programming))
