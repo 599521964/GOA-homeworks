a7a98ecc088e35ba16cd57fba1c82728240776b0
599521964 <giorgimelikidze71@gmail.com>
2024-06-08 01:09:02 +0400

    sixth Assignment

--- day 06/homework/lesson 06.py
+++ day 06/homework/lesson 06.py
@@ -1,7 +1,8 @@
+# Registration simulation
 #input  ნიშნავს შეტანას 
 #output ნიშნავს გამოტანას
-num1 = input("please enter your name: ") #num1 შემაქვს სახელი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
-num2 = input("please enter your surname: ") #num2 შემაქვს გვარი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
-num3 = input("please enter  your age: ") #num3 შემაქვს ასაკი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
-num4 = input("please enter second emile: ") #num4 შემაქვს ემაილი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
-print("result:",num1 + num2 + num3 + num4) #print ასრულებს ფუნქციას გამოტანას output და კრებავს ყველა ციფრს
+user_firstname = input("please enter your firstname: ") #num1 შემაქვს სახელი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
+user_lastname = input("please enter your lastname: ") #num2 შემაქვს გვარი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
+user_age= input("please enter  your age: ") #num3 შემაქვს ასაკი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
+user_email = input("please enter second emile: ") #num4 შემაქვს ემაილი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
+print("Your inputed informacion is:",user_firstname, user_lastname, user_age, user_email) #print ასრულებს ფუნქციას გამოტანას output გამოაქვს ეკრანზე
