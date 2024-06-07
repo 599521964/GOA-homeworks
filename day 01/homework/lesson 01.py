eae9847dfc26c131e7030a5f1274cbe49a460f0b
599521964 <giorgimelikidze71@gmail.com>
2024-06-08 00:42:09 +0400

    Sixth Assignment

---
+++ day 01/homework/lesson 01.py
@@ -0,0 +1,7 @@
+#input  ნიშნავს შეტანას 
+#output ნიშნავს გამოტანას
+num1 = int(input("please enter first number: ")) #num1 შემაქვს რიცხვი 10 აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს ბროგრამა
+num2 = int(input("please enter second number: ")) #num2 შემაქვს რიცხი 10 აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს ბროგრამა
+num3 = int(input("please enter  number3: ")) #num3 შემაქვს რიცხი 10 აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს ბროგრამა
+num4 = int(input("please enter second number4: ")) #num4 შემაქვს რიცხი 10 აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს ბროგრამა
+print("result:",num1 + num2 + num3 + num4) #print ასრულებს ფუნქციას გამოტანას output და კრებავს ყველა ციფრს
---
+++ day 06/homework/lesson 06.py
@@ -0,0 +1,7 @@
+#input  ნიშნავს შეტანას 
+#output ნიშნავს გამოტანას
+num1 = input("please enter your name: ") #num1 შემაქვს სახელი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
+num2 = input("please enter your surname: ") #num2 შემაქვს გვარი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
+num3 = input("please enter  your age: ") #num3 შემაქვს ასაკი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
+num4 = input("please enter second emile: ") #num4 შემაქვს ემაილი აქ ვიყენებ input-ს და ინფუთის სავალებით ვანიუშნებ როგორ გამოიყენოს პროგრამა
+print("result:",num1 + num2 + num3 + num4) #print ასრულებს ფუნქციას გამოტანას output და კრებავს ყველა ციფრს
