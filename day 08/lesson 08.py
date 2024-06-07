2881b2bfcd0eb0b78409575bb6de32694aa9977e
599521964 <giorgimelikidze71@gmail.com>
2024-06-08 01:37:13 +0400

    number eight

---
+++ day 08/lesson 08.py
@@ -0,0 +1,7 @@
+#input შეტანა
+#output გამოტანა
+num1 = int(input("please enter first numeber"))#ვინახავთ ციფრს input-ში
+num2 = int(input("please enter second numeber"))#ვინახავთ ციფრს input-ში
+num3 = int(input("please enter third numeber"))#ვინახავთ ციფრს input-ში
+num4 = int(input("please enter fourth numeber"))#ვინახავთ ციფრს input-ში
+print("result: ",num1+num2+num3+num4)#print აკეთებს ფუნქცის შეჯამებას
