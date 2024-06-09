5daa9a14ab0ded38e64caf81d2c4b17ddd1db003
599521964 <giorgimelikidze71@gmail.com>
2024-06-09 21:24:14 +0400

    The seventeenth task

---
+++ day 17/homework/main.py
@@ -0,0 +1,22 @@
+
+#budget = int(input("please enter your budget: "))
+
+#print=(budget > 50 or budget <= 100) # 50 მეტია ჩვიდმეტზე და არ არის ტოლი ანუ false,  100 მეტია ჩვიდმეტზე და არ არის ტოლი ანუ false  
+
+print(bool(-5.5))
+
+#control flow - დინების კონტროლი
+#sequence - თანმიმდერობა
+#iteraction - იტერაცია (გამეოება, ციკლი)
+#selection - არჩევა
+
+print("start")
+
+for i in range (10):
+    print("george")
+
+num = 3 
+if num ==5:
+    print("you won!")
+else:
+    print("you lost")
