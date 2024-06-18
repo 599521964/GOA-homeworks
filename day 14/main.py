74a1b1472be8fab4d325aafa44df23fa6e4bcf8e
599521964 <giorgimelikidze71@gmail.com>
2024-06-18 10:55:35 +0400

    Eighteenth task

---
+++ day 14/main.py
@@ -0,0 +1,21 @@
+#num1 = float(input("please enter first number: " ))
+#num2 = float(input("please enter second number:" ))
+
+#print(num1+num2)
+#print(num1-num2)
+#print(num1*num2)
+#print(num1/num2)
+
+#integer (int)_ -5, 5, 10, 12, 13
+#float_ -5.3, 5.4, 10.8, -12.3
+# str_"george", "5.4", "10", "-5"
+
+
+#print(str(5) + 5)
+
+
+print(5 > 5)
+print(5 < 5)
+print(5 >= 5)
+print(5 <= 5)
+print(5 == 5)
