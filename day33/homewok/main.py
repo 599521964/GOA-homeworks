c867f5fc64a34397d1f0a34484fd665b17ef9bca
599521964 <giorgimelikidze71@gmail.com>
2024-07-06 20:37:51 +0400

    '33'

---
+++ day33/homewok/main.py
@@ -0,0 +1,11 @@
+#max,მაქსიმალური  min,მინიმალური  sum შეჯამება
+
+numbers = [1,2,14,0,5,6,-8]
+
+max_number = numbers[0]
+
+for number in numbers: 
+    if max_number < number:  
+        max_number = number
+
+print(max_number)
